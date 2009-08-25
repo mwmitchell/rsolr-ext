0983df766837345e57b5233e5e5e6045011f2753
Matt Mitchell <goodieboy@gmail.com>
2009-08-25 13:31:45 -0400

    playing around with Model

--- test/model_test.rb
+++ test/model_test.rb
@@ -4,12 +4,26 @@ require 'helper'
 
 class RSolrExtModelTest < Test::Unit::TestCase
   
-  class DocX
+  class Book
+    
     include RSolr::Ext::Model
+    
+    def self.find_by_author(author, params={})
+      p = {:queries=>author, :qf=>:author_name, :qt=>:search}.merge(params)
+      find p
+    end
+    
+    def view
+      'blah'
+    end
+    
   end
   
   test "" do
-    
+    result = Book.find_by_author 'Jeffreys'
+    result.docs.each do |d|
+      puts d.view
+    end
   end
   
 end
