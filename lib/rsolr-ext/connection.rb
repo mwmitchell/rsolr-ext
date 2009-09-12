2d434354ff08a3a60b3e35771ce300e176fac81f
Matt Mitchell <goodieboy@gmail.com>
2009-09-11 22:32:37 -0400

    added support for POST requests when using the #find method

--- lib/rsolr-ext/connection.rb
+++ lib/rsolr-ext/connection.rb
@@ -12,9 +12,12 @@ module RSolr::Ext::Connection
   # mappings are available (everything else gets passed to solr).
   # Returns a new RSolr::Ext::Response::Base object.
   def find *args
+    # remove the handler arg - the first, if it is a string OR set default
     path = args.first.is_a?(String) ? args.shift : '/select'
-    params = args.pop || {}
-    response = self.request path, RSolr::Ext::Request.map(params)
+    # remove the params - the first, if it is a Hash OR set default
+    params = args.first.kind_of?(Hash) ? args.shift : {}
+    # send path, map params and send the rest of the args along
+    response = self.request path, RSolr::Ext::Request.map(params), *args
     RSolr::Ext::Response::Base.new(response)
   end
   
