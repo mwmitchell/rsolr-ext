587fdb5fd5003b8e9c2c3b269e331da0ae4b8218
Jason Ronallo <jronallo@gmail.com>
2010-08-02 10:46:16 +0800

    updating to work with spellcheck in solr 1.4

--- rsolr-ext.gemspec
+++ rsolr-ext.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{rsolr-ext}
-  s.version = "0.12.0"
+  s.version = "0.12.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Matt Mitchell"]
-  s.date = %q{2010-02-09}
+  s.date = %q{2010-08-01}
   s.description = %q{A query/response extension lib for RSolr}
   s.email = %q{goodieboy@gmail.com}
   s.extra_rdoc_files = [
@@ -34,12 +34,12 @@ Gem::Specification.new do |s|
   s.homepage = %q{http://github.com/mwmitchell/rsolr-ext}
   s.rdoc_options = ["--charset=UTF-8"]
   s.require_paths = ["lib"]
-  s.rubygems_version = %q{1.3.5}
+  s.rubygems_version = %q{1.3.7}
   s.summary = %q{A query/response extension lib for RSolr}
   s.test_files = [
     "spec/rsolr-ext_spec.rb",
-     "spec/spec.opts",
      "spec/spec_helper.rb",
+     "spec/spec.opts",
      "Rakefile"
   ]
 
@@ -47,7 +47,7 @@ Gem::Specification.new do |s|
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
     s.specification_version = 3
 
-    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
       s.add_development_dependency(%q<rspec>, [">= 1.2.9"])
       s.add_runtime_dependency(%q<rsolr>, [">= 0.12.1"])
     else
--- spec/rsolr-ext_spec.rb
+++ spec/rsolr-ext_spec.rb
@@ -1,15 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe RSolr::Ext do
-  
+
   context RSolr::Client do
-    
+
     let(:client){RSolr.connect}
-    
+
     it 'should now have a #find method' do
       client.should respond_to(:find)
     end
-    
+
     it 'should produce results from the #find method' do
       c = client
       c.should_receive(:request).
@@ -18,7 +18,7 @@ describe RSolr::Ext do
       response = c.find :page=>3, :per_page=>10, :q=>'*:*'#, :page=>1, :per_page=>10
       response.should be_a(Mash)
     end
-    
+
     it 'should call the #find method with a custom request handler' do
       c = client
       expected_response = {'response'=>{'docs' => []}, 'responseHeader' => {}}
@@ -32,7 +32,7 @@ describe RSolr::Ext do
       response = c.find '/select', :q=>'*:*'
       response.raw[:path].should match(/\/select/)
     end
-    
+
     it 'should be ok' do
       c = client
       c.should_receive(:request).
@@ -42,7 +42,7 @@ describe RSolr::Ext do
       response.should respond_to(:ok?)
       response.ok?.should == true
     end
-    
+
     it 'should call the #luke method' do
       c = client
       c.should_receive(:request).
@@ -54,7 +54,7 @@ describe RSolr::Ext do
       info.should have_key('index')
       info.should have_key('info')
     end
-    
+
     it 'should forwad #ping? calls to the connection' do
       client.connection.should_receive(:request).
         with('/admin/ping', :wt => :ruby ).
@@ -71,9 +71,9 @@ describe RSolr::Ext do
         and_raise( RSolr::RequestError.new("Solr Response: pingQuery_not_configured_consider_registering_PingRequestHandler_with_the_name_adminping_instead__") )
         lambda { client.ping? }.should raise_error( RSolr::RequestError )
     end
-    
+
   end
-  
+
   context 'requests' do
 
     it 'should create a valid request' do
@@ -94,26 +94,26 @@ describe RSolr::Ext do
       solr_params['facet.field'].should == ['cat', 'blah']
       solr_params[:facet].should == true
     end
-    
+
     it 'should map fq using the phrase_filters mapping' do
       solr_params = RSolr::Ext::Request.map(
         :phrase_filters=>{:manu=>['Apple', 'ASG'], :color=>['red', 'blue']}
       )
-      
+
       solr_params[:fq].size.should == 4
       solr_params[:fq].should include("color:\"red\"")
       solr_params[:fq].should include("color:\"blue\"")
       solr_params[:fq].should include("manu:\"Apple\"")
       solr_params[:fq].should include("manu:\"ASG\"")
-      
+
     end
-    
+
     it 'should map :filters and :phrase_filters while keeping an existing :fq' do
       solr_params = RSolr::Ext::Request.map(
         :fq => 'blah blah',
         :phrase_filters=>{:manu=>['Apple', 'ASG'], :color=>['red', 'blue']}
       )
-      
+
       solr_params[:fq].size.should == 5
       solr_params[:fq].should include("blah blah")
       solr_params[:fq].should include("color:\"red\"")
@@ -130,22 +130,22 @@ describe RSolr::Ext do
         solr_params[:fq].size.should == 1
         solr_params[:fq].should include("range:[1940 TO 2020]")
     end
-    
+
   end
-  
+
   context 'response' do
-    
+
     def create_response
       raw_response = eval(mock_query_response)
       RSolr::Ext::Response::Base.new(raw_response, '/select', raw_response['params'])
     end
-    
+
     it 'should create a valid response' do
       r = create_response
       r.should respond_to(:header)
       r.ok?.should == true
     end
-    
+
     it 'should have accurate pagination numbers' do
       r = create_response
       r.rows.should == 11
@@ -153,10 +153,10 @@ describe RSolr::Ext do
       r.start.should == 0
       r.docs.per_page.should == 11
     end
-    
+
     it 'should create a valid response class' do
       r = create_response
-    
+
       r.should respond_to(:response)
       r.ok?.should == true
       r.docs.size.should == 11
@@ -167,40 +167,40 @@ describe RSolr::Ext do
       r.should be_a(RSolr::Ext::Response::Docs)
       r.should be_a(RSolr::Ext::Response::Facets)
     end
-    
+
     it 'should create a doc with rsolr-ext methods' do
       r = create_response
-      
+
       doc = r.docs.first
       doc.has?(:cat, /^elec/).should == true
       doc.has?(:cat, 'elec').should_not == true
       doc.has?(:cat, 'electronics').should == true
-      
+
       doc.get(:cat).should == 'electronics, hard drive'
       doc.get(:xyz).should == nil
       doc.get(:xyz, :default=>'def').should == 'def'
     end
-    
+
     it 'should provide facet helpers' do
       r = create_response
       r.facets.size.should == 2
-    
+
       field_names = r.facets.collect{|facet|facet.name}
       field_names.include?('cat').should == true
       field_names.include?('manu').should == true
-      
+
       first_facet = r.facets.first
       first_facet.name.should == 'cat'
-      
+
       first_facet.items.size.should == 10
-      
+
       expected = "electronics - 14, memory - 3, card - 2, connector - 2, drive - 2, graphics - 2, hard - 2, monitor - 2, search - 2, software - 2"
       received = first_facet.items.collect do |item|
         item.value + ' - ' + item.hits.to_s
       end.join(', ')
-      
+
       expected.should == received
-      
+
       r.facets.each do |facet|
         facet.respond_to?(:name).should == true
         facet.items.each do |item|
@@ -208,35 +208,69 @@ describe RSolr::Ext do
           item.respond_to?(:hits).should == true
         end
       end
-      
+
     end
-    
+
     it 'should return the correct value when calling facet_by_field_name' do
       r = create_response
       facet = r.facet_by_field_name('cat')
       facet.name.should == 'cat'
     end
-    
+
     it 'should provide the responseHeader params' do
       raw_response = eval(mock_query_response)
       raw_response['responseHeader']['params']['test'] = :test
       r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', raw_response['params'])
       r.params['test'].should == :test
     end
-    
+
     it 'should provide the solr-returned params and "rows" should be 11' do
       raw_response = eval(mock_query_response)
       r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', {})
       r.params[:rows].to_s.should == '11'
     end
-    
+
     it 'should provide the ruby request params if responseHeader["params"] does not exist' do
       raw_response = eval(mock_query_response)
       raw_response.delete 'responseHeader'
       r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', :rows => 999)
       r.params[:rows].to_s.should == '999'
     end
-    
+
+    it 'should provide spelling suggestions for regular spellcheck results' do
+      raw_response = eval(mock_response_with_spellcheck)
+      r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', {})
+      r.spelling.words.should include("dell")
+      r.spelling.words.should include("ultrasharp")
+    end
+
+    it 'should provide spelling suggestions for extended spellcheck results' do
+      raw_response = eval(mock_response_with_spellcheck_extended)
+      r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', {})
+      r.spelling.words.should include("dell")
+      r.spelling.words.should include("ultrasharp")
+    end
+
+    it 'should provide no spelling suggestions when extended results and suggestion frequency is the same as original query frequency' do
+      raw_response = eval(mock_response_with_spellcheck_same_frequency)
+      r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', {})
+      r.spelling.words.should == []
+    end
+
+    it 'should provide spelling suggestions for a regular spellcheck results with a collation' do
+      raw_response = eval(mock_response_with_spellcheck_collation)
+      r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', {})
+      r.spelling.words.should include("dell")
+      r.spelling.words.should include("ultrasharp")
+    end
+
+    it 'should provide spelling suggestion collation' do
+      raw_response = eval(mock_response_with_spellcheck_collation)
+      r = RSolr::Ext::Response::Base.new(raw_response, '/catalog', {})
+      r.spelling.collation.should == 'dell ultrasharp'
+    end
+
   end
-  
+
 end
+
